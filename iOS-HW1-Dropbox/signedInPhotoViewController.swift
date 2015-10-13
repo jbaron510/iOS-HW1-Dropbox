6b0c11019b74a43cdc58cb84ff2d76bbcfaadac5
jbaron510 <jbaron@walmart.com>
2015-10-12 17:20:23 -0700

    Nearly all requirements included, still missing "return to welcome" code.

---
+++ iOS-HW1-Dropbox/signedInPhotoViewController.swift
@@ -0,0 +1,41 @@
+//
+//  signedInPhotoViewController.swift
+//  iOS-HW1-Dropbox
+//
+//  Created by Jon Baron on 10/12/15.
+//  Copyright © 2015 Walmart. All rights reserved.
+//
+
+import UIKit
+
+class signedInPhotoViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        scrollView.contentSize = CGSize(width: 320, height: 725)
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
